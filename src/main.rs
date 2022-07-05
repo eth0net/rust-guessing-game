f5b5918cd7c682dadb8c7370351f3e6f861e4902
Elliot Thomas <elliot@eth0net.io>
2022-07-05 21:13:49 +0100

    Implement secret number generation

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
+use rand::Rng;
 use std::io;
 
 fn main() {
-    println!("Guess the number");
+    println!("Guess the number!");
 
-    println!("Please input your guess.");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("The secret number is: {secret_number}");
+
+    println!("Please input your guess:");
 
     let mut guess = String::new();
 
